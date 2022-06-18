306cfb194cd6a773b4d595a993602b279902e08b
RenatoConfuorto <renato.confuorto@outlook.it>
2022-06-18 21:29:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to='/'>Home</router-link>
+      <router-link to='/second_page'>Second Page</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
